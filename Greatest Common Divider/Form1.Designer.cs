c658b755847c58f23c4e7b2c860a947de83dec10
Jikrebello <33652252+Jikrebello@users.noreply.github.com>
2021-05-19 23:31:17 +0200

    Add files via upload

---
+++ Greatest Common Divider/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace Greatest_Common_Divider
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnGreatestCommonDivider = new System.Windows.Forms.Button();
+            this.BtnMixedToImproper = new System.Windows.Forms.Button();
+            this.BtnLowestFormFractions = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnGreatestCommonDivider
+            // 
+            this.BtnGreatestCommonDivider.Location = new System.Drawing.Point(12, 12);
+            this.BtnGreatestCommonDivider.Name = "BtnGreatestCommonDivider";
+            this.BtnGreatestCommonDivider.Size = new System.Drawing.Size(221, 29);
+            this.BtnGreatestCommonDivider.TabIndex = 0;
+            this.BtnGreatestCommonDivider.Text = "Greatest Common Divider For 2 Numbers";
+            this.BtnGreatestCommonDivider.UseVisualStyleBackColor = true;
+            this.BtnGreatestCommonDivider.Click += new System.EventHandler(this.BtnGreatestCommonDivider_Click);
+            // 
+            // BtnMixedToImproper
+            // 
+            this.BtnMixedToImproper.Location = new System.Drawing.Point(10, 47);
+            this.BtnMixedToImproper.Name = "BtnMixedToImproper";
+            this.BtnMixedToImproper.Size = new System.Drawing.Size(221, 29);
+            this.BtnMixedToImproper.TabIndex = 1;
+            this.BtnMixedToImproper.Text = "Mixed To Improper";
+            this.BtnMixedToImproper.UseVisualStyleBackColor = true;
+            this.BtnMixedToImproper.Click += new System.EventHandler(this.BtnMixedToImproper_Click);
+            // 
+            // BtnLowestFormFractions
+            // 
+            this.BtnLowestFormFractions.Location = new System.Drawing.Point(10, 80);
+            this.BtnLowestFormFractions.Name = "BtnLowestFormFractions";
+            this.BtnLowestFormFractions.Size = new System.Drawing.Size(221, 29);
+            this.BtnLowestFormFractions.TabIndex = 2;
+            this.BtnLowestFormFractions.Text = "Add 2 Fractions to Lowest Form";
+            this.BtnLowestFormFractions.UseVisualStyleBackColor = true;
+            this.BtnLowestFormFractions.Click += new System.EventHandler(this.BtnLowestFormFractions_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(243, 116);
+            this.Controls.Add(this.BtnLowestFormFractions);
+            this.Controls.Add(this.BtnMixedToImproper);
+            this.Controls.Add(this.BtnGreatestCommonDivider);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnGreatestCommonDivider;
+        private System.Windows.Forms.Button BtnMixedToImproper;
+        private System.Windows.Forms.Button BtnLowestFormFractions;
+    }
+}
+
---
+++ Greatest Common Divider/Form1.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Windows.Forms;
+
+namespace Greatest_Common_Divider
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Question 1
+        private void BtnGreatestCommonDivider_Click(object sender, EventArgs e)
+        {
+            int numerator = Convert.ToInt32(Interaction.InputBox("Enter the Numerator", "Numerator"));
+            int denominator = Convert.ToInt32(Interaction.InputBox("Enter the Denominator", "Denominator"));
+
+            MessageBox.Show("The greatest Common Divider of " + numerator + " and " + denominator + " is " + GreatestCommonDivider(numerator, denominator));
+        }
+        private int GreatestCommonDivider(int _numerator, int _denominator)
+        {
+            int _temp;
+            int remainder;
+
+            // check and fix the order of the numerator and denominator if typed incorrectly
+            if (_denominator < _numerator)
+            {
+                _temp = _numerator;
+                _numerator = _denominator;
+                _denominator = _temp;
+            }
+
+            // do the euclid algorithm
+            while (true)
+            {
+                remainder = _numerator % _denominator;
+                if (remainder == 0)
+                    break;
+
+                _numerator = _denominator;
+                _denominator = remainder;
+            }
+
+            return _denominator;
+
+        }
+
+        // Question 2
+        private void BtnMixedToImproper_Click(object sender, EventArgs e)
+        {
+            int wholeNumber = Convert.ToInt32(Interaction.InputBox("Enter the Whole Number", "Whole Number"));
+            int numerator = Convert.ToInt32(Interaction.InputBox("Enter the Numerator", "Numerator"));
+            int denominator = Convert.ToInt32(Interaction.InputBox("Enter the Denominator", "Denominator"));
+
+            MessageBox.Show("The mixed fraction of " + wholeNumber + ", " + numerator + " and " + denominator + " is " + MixedToImproper(wholeNumber, numerator, denominator) + " over " + denominator);
+
+        }
+        private int MixedToImproper(int _wholeNumber, int _numerator, int _denominator)
+        {
+            return _ = _denominator * _wholeNumber + _numerator;
+        }
+
+        // Question 3
+        private void BtnLowestFormFractions_Click(object sender, EventArgs e)
+        {
+            int x1 = Convert.ToInt32(Interaction.InputBox("Enter the First Numbers' Numerator", ""));
+            int y1 = Convert.ToInt32(Interaction.InputBox("Enter the First Numbers' Denominator", ""));
+            
+            int x2 = Convert.ToInt32(Interaction.InputBox("Enter the Second Numbers' Denominator", ""));
+            int y2 = Convert.ToInt32(Interaction.InputBox("Enter the Second Numbers' Denominator", ""));
+
+            MessageBox.Show(AddingFractions(x1, y1, x2, y2));
+
+        }
+        private string AddingFractions(int _a, int _b, int _c, int _d)
+        {
+            int x = (_a * _d) + (_b * _c);
+            int y = _b * _d;
+
+            return _ = "The added fraction is " + x / GreatestCommonDivider(x, y) + "/" + y / GreatestCommonDivider(x, y);
+        }
+
+    }
+}
